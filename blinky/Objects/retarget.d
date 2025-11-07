b911edd97d85174c4bae13116887c9885a9d6edf
Artur Faizullin <127978732+JustIntenseC@users.noreply.github.com>
2025-11-07 16:42:31 +0300

    Add files via upload

---
+++ blinky/Objects/retarget.d
@@ -0,0 +1,11 @@
+./objects/retarget.o: Retarget\Retarget.c \
+  CMSIS\Device\Include\K1921VK035.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  CMSIS\Core\Include\core_cm4.h CMSIS\Core\Include\cmsis_version.h \
+  CMSIS\Core\Include\cmsis_compiler.h \
+  CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  CMSIS\Core\Include\mpu_armv7.h \
+  CMSIS\Device\Include\system_K1921VK035.h Retarget\retarget_conf.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h
---
+++ blinky/Objects/startup_k1921vk035.d
@@ -0,0 +1,1 @@
+.\objects\startup_k1921vk035.o: CMSIS\Device\Source\ARM\startup_K1921VK035.s
