21b0e5064845c611e17daa3e90a2171adadd2b7b
Artur Faizullin <127978732+JustIntenseC@users.noreply.github.com>
2025-11-07 16:43:12 +0300

    Delete blinky directory

--- blinky/Objects/system_k1921vk035.d
+++
@@ -1,9 +0,0 @@
-./objects/system_k1921vk035.o: CMSIS\Device\Source\system_K1921VK035.c \
-  CMSIS\Device\Include\system_K1921VK035.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  CMSIS\Device\Include\K1921VK035.h CMSIS\Core\Include\core_cm4.h \
-  CMSIS\Core\Include\cmsis_version.h CMSIS\Core\Include\cmsis_compiler.h \
-  CMSIS\Core\Include\cmsis_armclang.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
-  CMSIS\Core\Include\mpu_armv7.h
